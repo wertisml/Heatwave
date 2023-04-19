fa1c2d5acaaf316672b8e9e9f62390d100c42b45
wertisml <wertisml@appstate.edu>
2023-04-19 17:00:13 -0400

    Corrected the issues in the Heatwave_Calculator

--- ZIP_Code/5.Extremes_Calculations_Zip.R
+++ ZIP_Code/5.Extremes_Calculations_Zip.R
@@ -1,104 +1,53 @@
 library(data.table)
 library(dplyr)
-setwd("~/GRAM/HeatWave/Data")
+library(arrow)
+library(humidity)
 
-Heatwave_NC <- fread("C:\\Users\\owner\\Documents\\GRAM\\HeatWave\\Data\\NC_Heat_Index.csv")
-Heatwave <- fread("C:\\Users\\owner\\Documents\\GRAM\\HeatWave\\Data\\NC_Heatwave_ML.csv")
-#==============================================================================#
-#Combine .csv and clean the data
-#==============================================================================#
-
-#Heatwave <- rbind(Heatwave_NC, Heatwave_SC)
-
-Heatwave$Zip <- as.character(Heatwave$Zip)
-#Heatwave$year <- as.character(Heatwave$year)
-Heatwave$EHF[Heatwave$EHF <= 0] <- NA
+setwd("~/Heatwave/Data/Zip")
 
 #==============================================================================#
 #Calculate the 85th percentile EHF
 #==============================================================================#
 
-EHF85 <- Heatwave %>% 
-  group_by(Zip) %>% 
-  do(data.frame(t(quantile(.$EHF, probs = c(0.85), na.rm=T))))
-colnames(EHF85) <- c('Zip','EHF85')
-
-Heatwave <- merge(Heatwave, EHF85)
-Heatwave$Severe_occurance <- ifelse(Heatwave$EHF > 0, 1, 0)
-Heatwave$daynum <- 1
-Heatwave[is.na(Heatwave)] <- 0
-
-#==============================================================================#
-#Calculate the yearly data
-#==============================================================================#
-
-Hot_Yearly_Data <- Heatwave %>%
-  group_by(year)%>%
-  summarize(No_Heatwave = sum(daynum - Heatwave_condition),
-            Heatwave_days = sum(Heatwave_condition),
-            Severe_Heatwaves = sum(ifelse(EHF >= EHF85, 1, 0)),
-            Extreme_Heatwaves = sum(ifelse(EHF >= (3 * EHF85), 1, 0)),
-            mean_daily_Heatwaves = mean(Heatwave_condition)* 365, #Average number of heatwaves per county per year
-            sd_daily_Heatwaves = sd(Heatwave_condition),
-            mean_EHF = mean(EHF), #Per county per year
-            sd_EHF = sd(EHF),
-            Positive_EHF_occurance = sum(EHF > 0))  
-  
-#==============================================================================#
-#Calculate the county data
-#==============================================================================#
-
-Hot_County_Data <- Heatwave %>%
-            group_by(region_code)%>%
-            summarize(No_Heatwave = sum(daynum - Heatwave_condition),
-                      mean_No_Heatwave = sum(daynum - Heatwave_condition)/31,
-                      Total_Heatwaves = sum(Heatwave_condition),
-                      Severe_Heatwaves = sum(ifelse(EHF >= EHF85, 1, 0)),
-                      Extreme_Heatwaves = sum(ifelse(EHF >= (3 * EHF85), 1, 0)),
-                      mean_Heatwaves_yearly = mean(Heatwave_condition)*365, 
-                      sd_Heatwaves_yearly = sd(Heatwave_condition),
-                      mean_EHF = mean(EHF),
-                      sd_EHF = sd(EHF),
-                      EHF_yearly_occurance = sum(EHF > 0)/31)
+Heatwave <- left_join(open_dataset("NC_Heatwave.parquet") %>%
+                        mutate(Severe_occurance = ifelse(EHF > 0, 1, 0),
+                               daynum = 1) %>%
+                        collect() %>%
+                        drop_na(.),
+                      open_dataset("NC_Heatwave.parquet") %>% 
+                        select(Zip, EHF) %>%
+                        group_by(Zip) %>% 
+                        collect() %>%
+                        filter(EHF > 0) %>%
+                        do(data.frame(t(quantile(.$EHF, probs = c(0.85), na.rm=T)))) %>%
+                        rename(EHF85 = X85.),
+                      by = c("Zip"))
 
 #==============================================================================#
 #Calculate the daily information 
 #==============================================================================#
 
-Heat_Year <- Heatwave %>%
-  tibble() %>%
+Heat_Calc <- Heatwave %>%
   group_by(Zip) %>%
-  mutate(No_Heatwave = (daynum - Heatwave_condition),
-         Heatwave_days = (Heatwave_condition),
-         Severe_Heatwaves = (ifelse(EHF >= EHF85, 1, 0)),
+  mutate(Severe_Heatwaves = (ifelse(EHF >= EHF85, 1, 0)),
          Extreme_Heatwaves = (ifelse(EHF >= (3 * EHF85), 1, 0)),
          low_intensity = (ifelse(EHF > 0 & EHF < 1, 1, 0)),
          moderate_intensity = (ifelse(EHF >= 1 & EHF < 2, 1, 0)),  
          high_intensity = (ifelse(EHF >= 2, 1, 0)),
-         Above_95th = A95,
-         Above_97th = A97,
-         Above_99th = A99,
-         TAGVLag1 = lag(TAVG, order_by = Date),
-         TAGVLag2 = lag(TAVG, n = 2, order_by = Date),
-         TAGVLag3 = lag(TAVG, n = 3, order_by = Date),
-         TMINLag1 = lag(TMIN, order_by = Date),
-         TMINLag2 = lag(TMIN, n = 2, order_by = Date),
-         TMINLag3 = lag(TMIN, n = 3, order_by = Date),
-         TMAXLag1 = lag(TMAX, order_by = Date),
-         TMAXLag2 = lag(TMAX, n = 2, order_by = Date),
-         TMAXLag3 = lag(TMAX, n = 3, order_by = Date)) %>%
-  select(Date, Zip, No_Heatwave, Heatwave_days, Severe_Heatwaves, Extreme_Heatwaves, Above_95th, Above_97th, Above_99th,
-         low_intensity, moderate_intensity, high_intensity,TAVG, TMIN, TMAX, 
-         TAGVLag1, TAGVLag2,TAGVLag3, TMINLag1, TMINLag2, TMINLag3, TMAXLag1, TMAXLag2, TMAXLag3,
-         RH, Heat_Index, Discomfort_Index)
+         Above_95th = fifelse(TAVG > H95, 1, 0),
+         Above_97th  = fifelse(TAVG > H97, 1, 0),
+         Above_99th = fifelse(TAVG > H99, 1, 0),
+         RH = round(RH(TAVG, Tdmean, isK = FALSE), 2),
+         Heatwave = if_else(low_intensity > 0 | moderate_intensity > 0 | high_intensity > 0, 1, 0)) %>%
+  select(Date, Zip, TAVG, TMIN, TMAX, RH, EHF, EHI_sig, EHI_accl, 
+         Severe_Heatwaves, Extreme_Heatwaves, Above_95th, Above_97th, Above_99th, Heatwave,
+         low_intensity, moderate_intensity, high_intensity)
 
 #==============================================================================#
 #write to .csv
 #==============================================================================#
 
-fwrite(Hot_Yearly_Data, "Hot_Yearly_Data.csv")    
-fwrite(Hot_County_Data, "Hot_County_Data.csv") 
-fwrite(Heat_Year, "Heatwave_For_ML.csv")
+write_parquet(Heat_Calc, "Heatwave_Metrics.parquet")
 
 #==============================================================================#
 #Coldwaves
